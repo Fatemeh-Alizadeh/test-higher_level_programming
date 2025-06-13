a5223968740a54e2d3e40236616a84daf8554428
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-06-13 15:41:11 +1000

    Added task 2

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+comparison_text = ""
+
+if last_digit > 5:
+    comparison_text = "and is greater than 5"
+
+elif last_digit == 0:
+    comparison_text = "and is 0"
+elif last_digit < 6 and not 0:
+    comparison_text = "and is less than 6 and not 0"
+
+string = f"Last digit of {number} is {last_digit} {comparison_text}"
+print(string)
