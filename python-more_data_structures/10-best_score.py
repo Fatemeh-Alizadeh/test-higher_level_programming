a4021d0a63acf07b265f8262b19ee894b987b599
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-06-18 17:35:37 +1000

    Set and Dictionary task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,8 +5,6 @@ def best_score(a_dictionary):
         return None
     best = None
     for k in a_dictionary:
-        print(k)
-        print(a_dictionary[k])
         if best is None or a_dictionary[k] > a_dictionary[best]:
             best = k
     return best
