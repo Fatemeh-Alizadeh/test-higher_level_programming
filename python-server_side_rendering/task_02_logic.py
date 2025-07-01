42dd637c4c3ecf2abeb66bd444a3c03c8cb2c293
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-01 14:01:56 +1000

    server side rendering 02

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -16,6 +16,12 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route('/items', methods=['GET'])
+def items():
+    with open("items.json", 'r') as json_file:
+        data = json.load(json_file)
+        items = data['items']
+    return render_template("items.html", items=items)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
