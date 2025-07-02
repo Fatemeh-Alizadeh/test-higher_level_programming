5469cdf74c750f7d871194733182267a040abe75
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-03 09:30:53 +1000

    exception 01

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,6 +3,7 @@
 This module provides a function that divides all elements of a matrix.
 """
 
+
 def matrix_divided(matrix, div):
     """
         Divides all elements of a matrix by a number.
@@ -20,19 +21,23 @@ def matrix_divided(matrix, div):
                        or if div is not a number.
             ZeroDivisionError: If div is 0.
         """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if (not isinstance(matrix, list) or
+            not all(isinstance(row, list) for row in matrix)):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     row_length = None
     for row in matrix:
         if row_length is None:
             row_length = len(row)
         elif len(row) != row_length:
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError(
+                "Each row of the matrix must have the same size")
 
         for item in row:
             if not isinstance(item, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
