6a8084a7cd5b226bf00c33a9f5599d8754c61e40
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-02 13:14:13 +1000

    exceptions 03

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-    except Exception as e :
+    except ZeroDivisionError as e :
         result = None
     finally:
         print("Inside result: {}".format(result))
