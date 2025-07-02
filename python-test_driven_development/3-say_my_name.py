2a246caeff4887c31cda1bdba7dd0f5972c62f12
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-03 09:54:48 +1000

    exception 02

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module prints a string with firstname and lastname
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    This function prints a string with firstname and lastname
+    Args:
+    first_name (str)
+    last_name (str)
+    Return:
+        a string with firstname and lastname
+    Raises:
+        TypeError: if first_name or last_name are not a string
+
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    return f"My name is {first_name} {last_name}"
