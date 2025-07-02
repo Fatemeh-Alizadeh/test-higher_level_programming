bbba00b0aa806e73786adc19fae2c6c54a99a525
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-03 09:36:38 +1000

    exception 01

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -37,7 +37,8 @@ def matrix_divided(matrix, div):
         for item in row:
             if not isinstance(item, (int, float)):
                 raise TypeError(
-                    "matrix must be a matrix (list of lists) of integers/floats")
+                    "matrix must be a matrix "
+                    "(list of lists) of integers/floats")
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
