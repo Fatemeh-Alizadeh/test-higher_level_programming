1340bf21c8bca574bb738173a59030c4937ea3f0
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-02 13:23:35 +1000

    exceptions 021

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
 
         except (ValueError, TypeError) :
             continue
+        except IndexError:
+            raise
     print()
     return count
--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,10 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-    except ZeroDivisionError as e :
+    except (TypeError, NameError):
+        pass
+
+    except ZeroDivisionError :
         result = None
     finally:
         print("Inside result: {}".format(result))
