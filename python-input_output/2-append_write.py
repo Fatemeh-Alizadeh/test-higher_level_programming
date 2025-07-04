f6cbd1262c19a7c2e5fb98edccc5f6ef155a9e8d
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-04 11:47:56 +1000

    'Input/Output 01'

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,6 +4,7 @@ Appends a string to the end of a UTF-8 text file
 and returns the number of characters added.
 """
 
+
 def append_write(filename="", text=""):
     """
     Appends a string to the end of a UTF-8 text file.
