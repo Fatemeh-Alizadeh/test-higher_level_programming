8ae15e11c4f3590df3b80f8dfc18da04691fbf92
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-04 11:28:30 +1000

    'Input/Output 00'

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,7 +9,7 @@ def read_file(filename=""):
     Reads a UTF-8 encoded text file and prints its contents to stdout.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename (str): The name of the file to read.
     """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
