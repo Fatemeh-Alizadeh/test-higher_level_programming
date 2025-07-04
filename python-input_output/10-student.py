a27de20dca3f5556fa6e380d939563801a6fd676
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-04 13:22:47 +1000

    'Input/Output 10'

---
+++ python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Defines a student by first name, last name, and age.
+"""
+
+
+class Student:
+    """
+    Defines a student by first name, last name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance.
+
+        Args:
+            first_name
+            last_name
+            age (int):
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of the Student instance.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            new_dic = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    new_dic[attr] = self.__dict__[attr]
+            return new_dic
+
+        else:
+            return self.__dict__
