da2452dd6f2b905e50a014ebc0583fdc60f12938
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-07 10:53:43 +1000

    'serialization 03'

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
 import xml.etree.ElementTree as ET
+
 
 def serialize_to_xml(dictionary, filename):
     """
@@ -51,5 +51,3 @@ def deserialize_from_xml(filename):
     except Exception as e:
         print(f"Error reading XML: {e}")
         return {}
-
-
