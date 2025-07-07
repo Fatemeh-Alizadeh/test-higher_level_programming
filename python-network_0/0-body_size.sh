f42d82db4966ecaf2788418932aa5ade536e7214
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-07 13:23:19 +1000

    'Network 00'

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+# Check if a URL was provided
 if [ -z "$1" ]; then
   echo "Usage: $0 <URL>"
   exit 1
 fi
-
+# Send a request and get the size of the response body in bytes
 curl -s -o /dev/null -w "%{size_download}\n" "$1"
