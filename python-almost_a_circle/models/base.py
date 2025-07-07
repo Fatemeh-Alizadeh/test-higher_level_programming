248ca0f54f2b52b1cbb0cc8f9241979e8f5891a1
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-07 16:40:33 +1000

    'Almost a circle 01'

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,3 +1,20 @@
+"""
+Base class for managing id attribute across all future classes.
+"""
+
 
 class Base:
-    def __init__(self, **kwargs):
+    """Base class for managing id attribute across all future classes."""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Initialize the Base instance with an id.
+
+        Args:
+            id (int, optional): The id to assign to the instance. If None,
+                                an auto-incremented value is assigned.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
