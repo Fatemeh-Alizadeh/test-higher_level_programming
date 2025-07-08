8ec12e194a9b14cb2dc2369916218c3da82127e0
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-08 10:11:30 +1000

    'Almost a circle 03'

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,7 @@
 Rectangle class that inherits from Base.
 """
 
-from .base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -22,20 +22,20 @@ class Rectangle(Base):
             id (int, optional): The ID of the instance.
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
-            return self.__width
+        return self.__width
 
     @width.setter
     def width(self, value):
-        if not isinstance(self.__width, int):
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        elif self.__width <= 0:
+        elif value <= 0:
             raise ValueError('width must be > 0')
         else:
             self.__width = value
@@ -46,9 +46,9 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if not isinstance(self.__height, int):
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        elif self.__height <= 0:
+        elif value <= 0:
             raise ValueError('height must be > 0')
         else:
             self.__height = value
@@ -59,9 +59,9 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if not isinstance(self.__x, int):
+        if not isinstance(value, int):
             raise TypeError('x must be an integer')
-        elif self.__x < 0:
+        elif value < 0:
             raise ValueError('x must be >= 0')
         else:
             self.__x = value
@@ -72,9 +72,9 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if not isinstance(self.__y, int):
+        if not isinstance(value, int):
             raise TypeError('y must be an integer')
-        elif self.__y <= 0:
+        elif value < 0:
             raise ValueError('y must be >= 0')
         else:
             self.__y = value
