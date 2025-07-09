d26bd9f0549338a48fbbeb486e981322dd3218dc
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-09 12:03:25 +1000

    'almost a circle 16'

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,10 +38,10 @@ class Base:
 
         filename = f"{cls.__name__}.json"
         if list_objs is None or []:
-            return []
+            dict_list = []
         else:
             dict_list = [obj.to_dictionary() for obj in list_objs]
 
-            json_string = cls.to_json_string(dict_list)
-            with open(filename, 'w') as f:
-                return f.write(json_string)
+        json_string = cls.to_json_string(dict_list)
+        with open(filename, 'w') as f:
+            return f.write(json_string)
