38c69c8f8f561cd7aea5496a8e405fe81c5f31bc
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-09 12:22:23 +1000

    'almost a circle 18'

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -53,3 +53,19 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create a new instance with a dictionary."""
+
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = None
+
+        if dummy:
+            dummy.update(**dictionary)
+        return dummy
+
