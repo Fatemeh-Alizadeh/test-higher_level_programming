169d901eb75d7ecb335ff015f8a60d7f69504cbf
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-09 10:02:44 +1000

    'almost a circle 11'

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,6 +23,16 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         Return the string representation of the Square.
@@ -31,3 +41,17 @@ class Square(Rectangle):
             [Square] (<id>) <x>/<y> - <size>
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+
+if __name__ == "__main__":
+
+    s1 = Square(5)
+    print(s1)
+    print(s1.size)
+    s1.size = 10
+    print(s1)
+
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
