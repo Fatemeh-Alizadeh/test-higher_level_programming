dda153a6b4ee3f1d31a66c94c1288841a408a773
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-11 13:33:28 +1000

    'RestfulAPI 03'

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -34,8 +34,7 @@ class requestHendler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            error_message = {"error": "Endpoint not found"}
-            self.wfile.write(json.dumps(error_message).encode('utf-8'))
+            self.wfile.write(b"Endpoint not found")
 
 
 server = HTTPServer(('', 8000), requestHendler)
